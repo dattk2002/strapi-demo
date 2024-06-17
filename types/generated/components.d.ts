310864355d1241bfba1764c9d493074299593db1
kimdat0705@gmail.com <kimdat0705@gmail.com>
2024-06-17 10:29:44 +0700

    init code

---
+++ types/generated/components.d.ts
@@ -0,0 +1,105 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface BlocksHero extends Schema.Component {
+  collectionName: 'components_blocks_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    heading: Attribute.String;
+    text: Attribute.Text;
+    link: Attribute.Component<'elements.button-link', true>;
+    image: Attribute.Media<'images'>;
+  };
+}
+
+export interface BlocksPricing extends Schema.Component {
+  collectionName: 'components_blocks_pricings';
+  info: {
+    displayName: 'Pricing';
+  };
+  attributes: {
+    name: Attribute.String;
+    description: Attribute.String;
+    plan: Attribute.Component<'elements.pricing-card', true>;
+  };
+}
+
+export interface BlocksRow extends Schema.Component {
+  collectionName: 'components_blocks_rows';
+  info: {
+    displayName: 'Row';
+  };
+  attributes: {
+    card: Attribute.Component<'elements.card', true>;
+  };
+}
+
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    type: Attribute.Enumeration<['PRIMARY ', 'SECONDARY']>;
+  };
+}
+
+export interface ElementsCard extends Schema.Component {
+  collectionName: 'components_elements_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {
+    image: Attribute.Media<'images'>;
+    heading: Attribute.String;
+    description: Attribute.Text;
+  };
+}
+
+export interface ElementsPricingCard extends Schema.Component {
+  collectionName: 'components_elements_pricing_cards';
+  info: {
+    displayName: 'Pricing Card';
+  };
+  attributes: {
+    planType: Attribute.String;
+    planPrice: Attribute.String;
+    isFeatured: Attribute.Boolean & Attribute.DefaultTo<false>;
+    services: Attribute.Relation<
+      'elements.pricing-card',
+      'oneToMany',
+      'api::service.service'
+    >;
+    link: Attribute.Component<'elements.button-link'>;
+  };
+}
+
+export interface SeoMetaData extends Schema.Component {
+  collectionName: 'components_seo_meta_data';
+  info: {
+    displayName: 'Meta Data';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+    metaImage: Attribute.Media<'images'>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'blocks.hero': BlocksHero;
+      'blocks.pricing': BlocksPricing;
+      'blocks.row': BlocksRow;
+      'elements.button-link': ElementsButtonLink;
+      'elements.card': ElementsCard;
+      'elements.pricing-card': ElementsPricingCard;
+      'seo.meta-data': SeoMetaData;
+    }
+  }
+}
